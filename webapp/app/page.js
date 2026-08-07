09e2b429ff286c4bd68b8417eff9c1eb906ef02e
Kebin Maharjan <mhrznkebin@gmail.com>
2026-08-07 10:15:08 +0545

    Render home content at root instead of server redirect (static export)

--- webapp/app/page.js
+++ webapp/app/page.js
@@ -1,5 +1,24 @@
-import { redirect } from "next/navigation";
+import React from "react";
+import { Navbar9 } from "@/components/Navbar9";
+import { Header26 } from "./home/components/Header26";
+import { Layout239 } from "./home/components/Layout239";
+import { Layout89 } from "./home/components/Layout89";
+import { Layout419 } from "./home/components/Layout419";
+import { Layout502 } from "./home/components/Layout502";
+import { Cta25 } from "./home/components/Cta25";
+import { Footer4 } from "@/components/Footer4";
 
 export default function Home() {
-  redirect("/home");
+  return (
+    <div>
+      <Navbar9 />
+      <Header26 />
+      <Layout239 />
+      <Layout89 />
+      <Layout419 />
+      <Layout502 />
+      <Cta25 />
+      <Footer4 />
+    </div>
+  );
 }
