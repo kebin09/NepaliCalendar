60d5ac55175eade0f1bd01708362bd8b5d31312a
Kebin Maharjan <mhrznkebin@gmail.com>
2026-08-07 10:04:38 +0545

    Include data JSON in serverless tracing for Vercel

--- webapp/next.config.mjs
+++ webapp/next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    outputFileTracingIncludes: {
+      "/posts": ["./data/posts.json"],
+      "/podcasts": ["./data/podcasts.json"],
+    },
+  },
 };
 
 export default nextConfig;
